9778847c28fcd7eae267e1bf13199e49dba66fa0
Jonathan Freed <jon@freed.dev>
2024-03-29 13:09:23 -0400

    INIT Push to GIT

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PAN Prisma Testing',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Jonathan Freed',
+    author_email='jon@freed.dev',
+    description='testing API code'
+)
